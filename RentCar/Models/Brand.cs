1f8c61339d17c710328992eec4025ed6f221cfde
Jakub <96346556+Khazaxa@users.noreply.github.com>
2023-06-12 12:07:09 +0200

    Create Folders

--- RentCar/Models/Brand.cs
+++ RentCar/Models/Brand.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace RentCar;
+namespace RentCar.Models;
 
 public partial class Brand
 {
--- RentCar/Models/CarRentContext.cs
+++ RentCar/Models/CarRentContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace RentCar;
+namespace RentCar.Models;
 
 public partial class CarRentContext : DbContext
 {
