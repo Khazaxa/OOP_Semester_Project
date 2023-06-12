25f373cef0d19e8de5aaed32b20676cc56131b52
Jakub <96346556+Khazaxa@users.noreply.github.com>
2023-06-12 12:41:23 +0200

    Center Views

--- RentCar/Views/MainWindow.xaml.cs
+++ RentCar/Views/MainWindow.xaml.cs
@@ -1,28 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RentCar.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RentCar
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
+
+        private void AddCar_Click(object sender, RoutedEventArgs e)
+        {
+            AddCarView addCarView = new AddCarView();
+            addCarView.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            addCarView.ShowDialog();
+        }
+
+        private void RentCar_Click(object sender, RoutedEventArgs e)
+        {
+            RentCarView rentCarView = new RentCarView();
+            rentCarView.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            rentCarView.ShowDialog();
+        }
+
+        private void ReturnCar_Click(object sender, RoutedEventArgs e)
+        {
+            ReturnCarView returnCarView = new ReturnCarView();
+            returnCarView.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            returnCarView.ShowDialog();
+        }
+
+        private void AvailableCars_Click(object sender, RoutedEventArgs e)
+        {
+            AvailableCarsView availableCarsView = new AvailableCarsView();
+            availableCarsView.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            availableCarsView.ShowDialog();
+        }
+
     }
 }
