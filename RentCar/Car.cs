646daf1fc9bb7d938a5975449aef02138dfaf0ba
Jakub <96346556+Khazaxa@users.noreply.github.com>
2023-06-12 11:31:37 +0200

    Finally Connect To DB

---
+++ RentCar/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentCar;
+
+public partial class Car
+{
+    public int Id { get; set; }
+
+    public int BrandId { get; set; }
+
+    public string Model { get; set; } = null!;
+
+    public int Year { get; set; }
+
+    public string RegistrationNumber { get; set; } = null!;
+
+    public virtual Brand Brand { get; set; } = null!;
+
+    public virtual ICollection<Rental> Rentals { get; set; } = new List<Rental>();
+}
